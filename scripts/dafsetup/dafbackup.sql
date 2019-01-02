7fd7974def4dffc78bbd9445f64ca5c70c248333
Adam Bowen <adam.bowen@delphix.com>
2019-01-02 17:31:44 -0500

    updated dafdb sql

--- scripts/dafsetup/dafbackup.sql
+++ scripts/dafsetup/dafbackup.sql
@@ -70,6 +70,20 @@ SET default_with_oids = false;
 -- Name: billings; Type: TABLE; Schema: public; Owner: :user
 --
 
+-- CREATE TABLE public.billings (
+--     id integer NOT NULL,
+--     patient_id integer NOT NULL,
+--     ccnum character varying(32) NOT NULL,
+--     cctype character varying(55) NOT NULL,
+--     ccexpmonth integer NOT NULL,
+--     ccexpyear integer NOT NULL,
+--     address1 character varying(255) NOT NULL,
+--     address2 character varying(255),
+--     city character varying(255) NOT NULL,
+--     state character varying(4) NOT NULL,
+--     zip character varying(12) NOT NULL
+-- );
+
 CREATE TABLE public.billings (
     id integer NOT NULL,
     patient_id integer NOT NULL,
@@ -81,7 +95,9 @@ CREATE TABLE public.billings (
     address2 character varying(255),
     city character varying(255) NOT NULL,
     state character varying(4) NOT NULL,
-    zip character varying(12) NOT NULL
+    zip character varying(12) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
 
 
@@ -107,9 +123,64 @@ ALTER TABLE public.billings_id_seq OWNER TO :user;
 
 ALTER SEQUENCE public.billings_id_seq OWNED BY public.billings.id;
 
+
+--
+-- Name: databasechangelog; Type: TABLE; Schema: public; Owner: :user
+--
+
+CREATE TABLE public.databasechangelog (
+    id character varying(255) NOT NULL,
+    author character varying(255) NOT NULL,
+    filename character varying(255) NOT NULL,
+    dateexecuted timestamp without time zone NOT NULL,
+    orderexecuted integer NOT NULL,
+    exectype character varying(10) NOT NULL,
+    md5sum character varying(35),
+    description character varying(255),
+    comments character varying(255),
+    tag character varying(255),
+    liquibase character varying(20),
+    contexts character varying(255),
+    labels character varying(255),
+    deployment_id character varying(10)
+);
+
+
+ALTER TABLE public.databasechangelog OWNER TO :user;
+
+--
+-- Name: databasechangeloglock; Type: TABLE; Schema: public; Owner: :user
+--
+
+CREATE TABLE public.databasechangeloglock (
+    id integer NOT NULL,
+    locked boolean NOT NULL,
+    lockgranted timestamp without time zone,
+    lockedby character varying(255)
+);
+
+
+ALTER TABLE public.databasechangeloglock OWNER TO :user;
+
 --
 -- Name: patients; Type: TABLE; Schema: public; Owner: :user
 --
+
+-- CREATE TABLE public.patients (
+--     id integer NOT NULL,
+--     firstname character varying(255) NOT NULL,
+--     middlename character varying(255) NOT NULL,
+--     lastname character varying(255) NOT NULL,
+--     ssn character varying(12) NOT NULL,
+--     dobyear smallint NOT NULL,
+--     dobmonth smallint NOT NULL,
+--     dobday smallint NOT NULL,
+--     address1 character varying(255) NOT NULL,
+--     address2 character varying(255) NOT NULL,
+--     city character varying(255) NOT NULL,
+--     state character varying(4) NOT NULL,
+--     zip character varying(12) NOT NULL
+-- );
 
 CREATE TABLE public.patients (
     id integer NOT NULL,
@@ -124,9 +195,10 @@ CREATE TABLE public.patients (
     address2 character varying(255) NOT NULL,
     city character varying(255) NOT NULL,
     state character varying(4) NOT NULL,
-    zip character varying(12) NOT NULL
+    zip character varying(12) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
-
 
 ALTER TABLE public.patients OWNER TO :user;
 
@@ -155,6 +227,16 @@ ALTER SEQUENCE public.patients_id_seq OWNED BY public.patients.id;
 -- Name: payments; Type: TABLE; Schema: public; Owner: :user
 --
 
+-- CREATE TABLE public.payments (
+--     id integer NOT NULL,
+--     patient_id integer NOT NULL,
+--     amount integer NOT NULL,
+--     authcode character varying(36) NOT NULL,
+--     currency character varying(4) NOT NULL,
+--     captured boolean DEFAULT false NOT NULL,
+--     type character varying(55) NOT NULL
+-- );
+
 CREATE TABLE public.payments (
     id integer NOT NULL,
     patient_id integer NOT NULL,
@@ -162,9 +244,10 @@ CREATE TABLE public.payments (
     authcode character varying(36) NOT NULL,
     currency character varying(4) NOT NULL,
     captured boolean DEFAULT false NOT NULL,
-    type character varying(55) NOT NULL
+    type character varying(55) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
-
 
 ALTER TABLE public.payments OWNER TO :user;
 
@@ -193,12 +276,19 @@ ALTER SEQUENCE public.payments_id_seq OWNED BY public.payments.id;
 -- Name: records; Type: TABLE; Schema: public; Owner: :user
 --
 
+-- CREATE TABLE public.records (
+--     id integer NOT NULL,
+--     patient_id integer NOT NULL,
+--     type character varying(255) NOT NULL
+-- );
+
 CREATE TABLE public.records (
     id integer NOT NULL,
     patient_id integer NOT NULL,
-    type character varying(255) NOT NULL
+    type character varying(255) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
-
 
 ALTER TABLE public.records OWNER TO :user;
 
@@ -232,9 +322,10 @@ CREATE TABLE public.users (
     username character varying(255) NOT NULL,
     firstname character varying(255) NOT NULL,
     lastname character varying(255) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
     password character varying(255) NOT NULL
 );
-
 
 ALTER TABLE public.users OWNER TO :user;
 
@@ -298,7 +389,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: billings; Type: TABLE DATA; Schema: public; Owner: :user
 --
 
-\COPY public.billings (id, patient_id, ccnum, cctype, ccexpmonth, ccexpyear, address1, address2, city, state, zip) FROM '/tmp/dafsetup/billings.csv'DELIMITER ',' CSV HEADER;;
+\COPY public.billings (id, patient_id, ccnum, cctype, ccexpmonth, ccexpyear, address1, address2, city, state, zip, created_at, updated_at) FROM '/tmp/dafsetup/billings.csv'DELIMITER ',' CSV HEADER;;
 
 
 --
@@ -312,7 +403,7 @@ SELECT pg_catalog.setval('public.billings_id_seq', 1, false);
 -- Data for Name: patients; Type: TABLE DATA; Schema: public; Owner: :user
 --
 
-\COPY public.patients (id, firstname, middlename, lastname, ssn, dobyear, dobmonth, dobday, address1, address2, city, state, zip) FROM '/tmp/dafsetup/patients.csv'DELIMITER ',' CSV HEADER;
+\COPY public.patients (id, firstname, middlename, lastname, ssn, dobyear, dobmonth, dobday, address1, address2, city, state, zip, created_at, updated_at) FROM '/tmp/dafsetup/patients.csv'DELIMITER ',' CSV HEADER;
 
 
 --
@@ -326,7 +417,7 @@ SELECT pg_catalog.setval('public.patients_id_seq', 1, false);
 -- Data for Name: payments; Type: TABLE DATA; Schema: public; Owner: :user
 --
 
-\COPY public.payments (id, patient_id, amount, authcode, currency, captured, type) FROM '/tmp/dafsetup/payments.csv'DELIMITER ',' CSV HEADER;
+\COPY public.payments (id, patient_id, amount, authcode, currency, captured, type, created_at, updated_at) FROM '/tmp/dafsetup/payments.csv'DELIMITER ',' CSV HEADER;
 
 
 --
@@ -340,7 +431,7 @@ SELECT pg_catalog.setval('public.payments_id_seq', 1, false);
 -- Data for Name: records; Type: TABLE DATA; Schema: public; Owner: :user
 --
 
-\COPY public.records (id, patient_id, type) FROM '/tmp/dafsetup/records.csv'DELIMITER ',' CSV HEADER;;
+\COPY public.records (id, patient_id, type, created_at, updated_at) FROM '/tmp/dafsetup/records.csv'DELIMITER ',' CSV HEADER;;
 
 
 --
