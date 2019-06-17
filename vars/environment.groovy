fad6c891994ef8b852f9aa28c0759cdc8b35efb1
Adam Bowen <adam.bowen@delphix.com>
2019-06-17 06:48:11 -0400

    enable public IP to shutdown vdbs.

--- vars/environment.groovy
+++ vars/environment.groovy
@@ -42,6 +42,7 @@ def environmentTest(){
 def terraformBuild(staged){
   sh """#!/bin/bash
     { set -x; } 2>/dev/null
+    echo 'associate_public_ip_address = "true"' > enable_public_ip.auto.tfvars 
     CURRENT_UID=\$(id -u):\$(id -g) docker-compose run tcw deploy -auto-approve -var "staged=${staged}"
   """
 }
